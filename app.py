74c9ac662c326a923c264ade2f7251c71652d0d5
dusana2508 <duca2508@gmail.com>
2018-04-14 18:32:34 +0200

    Implementiran kostur kontrolne aplikacije za News-me (/configuration<client>, /users)

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask (__name__)
+
+@app.route('/')
+def homepage():
+  return render_template('index.html')
+
+@app.route('/configuration/<client>')
+def getMeCofiguration(client):
+  return render_template('configuration.html', name = client)
+
+@app.route('/configuration')
+def configurationWithOutClient():
+    return render_template('configuration.html')
+
+@app.route('/users')
+def getMeUsers():
+  return render_template('users.html')
+
 app.run()
