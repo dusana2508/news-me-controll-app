81c64f6c8b097b88690b708271d61d108943e265
dusana2508 <duca2508@gmail.com>
2018-04-19 19:26:47 +0200

    Prikazivanje liste svih klijenata(aplikacije) po dizajnu na ruti /configuration

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template, redirect, url_for
+import requests
 
 app = Flask (__name__)
+
+configOption = requests.get('http://api-news-me.ml/configurations-with-options').json()
+
+config = configOption['currentConfigurations']
+configurationOptions = configOption['configurationOptions']
 
 @app.route('/')
 def homepage():
   return render_template('index.html')
 
-@app.route('/configuration/<client>')
-def getMeCofiguration(client):
-  return render_template('configuration.html', name = client)
+@app.route('/configuration')
+def getMeCofiguration():
+    return render_template('configuration.html', clients = config)
+ 
 
 @app.route('/configuration')
 def configurationWithOutClient():
